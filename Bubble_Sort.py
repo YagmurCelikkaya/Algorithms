e4c54d542d67a3f89d4c108a805512021d50e6ac
YagmurCelikkaya <ry.celikkaya@gmail.com>
2025-04-05 15:56:34 +0300

    Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,11 +1,13 @@
 # A bubble sort algorithm based on user input
 length = (int)(input("Please enter the length of your list : "))  # Get the length of the list
 BubbleList = []  # Create an empty list
+
 print("Please enter the number you want it")
 for i in range(length):  # Create a for loop to get the elements
     num = (int)(input(f"Number {i+1} : "))
     BubbleList.append(num)  # Add them to the list 
 print(f"Your list is {BubbleList}")  # Print the list
+
 choice = (input("Please enter 'yes' or 'no' if you want your list to be sorted : ")).lower()
 if choice == "yes":
     print("\nYour list is going to be sorted by using *Bubble Sort* method")
